7796ae6d7130a8d85adcbf954643d8617e1699e2
LK Local <kovic_leo@hotmail.com>
2020-05-13 22:49:53 -0300

    dia 2 #30díasdegráficos

--- code/02. graficar_lineas.R
+++ code/02. graficar_lineas.R
@@ -10,6 +10,7 @@
 library(tidyverse)
 library(readxl)
 library(here)
+library(RColorBrewer)
 
 # Leer los datos de defunciones 2018 - 2009
 url_defunciones_2018  <- "http://www.deis.msal.gov.ar/wp-content/uploads/2020/01/DefWeb18.csv"
@@ -28,25 +29,15 @@ dfDefuncionesPeriodo <- data.frame()
 for(anio in c(2009 : 2018)) {
   dfDefuncionesRaw <- readr::read_csv(get(paste0("url_defunciones_", anio))) %>% 
     mutate(anio = anio)
-  cat(paste0("\n --- Anio ", anio, "\nEl df leído tiene ", nrow(dfDefuncionesRaw),
-             "filas y ", nrow(dfDefuncionesRaw), " columnas.\n"))
-  dfDefuncionesPeriodo       <- rbind(dfDefuncionesPeriodo, dfDefuncionesRaw)
+  dfDefuncionesPeriodo <- rbind(dfDefuncionesPeriodo, dfDefuncionesRaw)
   rm(dfDefuncionesRaw)
 }
-
-# Guardar el dataset en un .rds
-# saveRDS(dfDefuncionesPeriodo, 
-#        file = here::here("data", "01 raw", "defunciones2009-2018.rds"))
 
 # Leer diccionario de variables
 url_diccionario  <- "http://www.deis.msal.gov.ar/wp-content/uploads/2019/01/DescDef1.xlsx"
 tmp              <-  tempfile(fileext = ".xlsx")
 download.file(url = url_diccionario, destfile = tmp, mode = "wb")
 diccionario      <- readxl::read_excel(tmp, sheet = "CODMUER", col_names = TRUE)
-
-# saveRDS(diccionario, 
-#        file = here::here("data", "01 raw", "diccionario.rds"))
-
 
 # Top 10 causas del periodo
 dfTop10causas <- dfDefuncionesPeriodo %>% 
@@ -61,15 +52,19 @@ df <- dfDefuncionesPeriodo %>%
   dplyr::mutate(CAUSA = factor(CAUSA, levels = dfTop10causas$CAUSA)) %>% 
   dplyr::group_by(anio, CAUSA) %>% 
   dplyr::summarise( n  = sum(CUENTA))  %>%
-  merge(., diccionario, by.x = "CAUSA", by.y = "CODIGO", all.x = TRUE) 
+  merge(., diccionario, by.x = "CAUSA", by.y = "CODIGO", all.x = TRUE) %>% 
+  dplyr::group_by(CAUSA) %>% 
+  mutate(sd = sd(n),
+         cv = 100 * sd(n) / mean(n),
+         etiquetas = stringr::str_wrap(VALOR, 35))
 
+# Ordenar los niveles de las etiquetas
+orden_etiquetas <- df %>% dplyr::filter(anio == 2018) %>% arrange( - n)
+df <- df %>% 
+  dplyr::mutate(etiquetas = 
+                  factor(etiquetas, levels = orden_etiquetas$etiquetas) )
 
-#   dplyr::mutate(
-#     destacado = factor(1 * (n == max(n) )),
-#     etiquetas = stringr::str_wrap(VALOR, 35)
-# )
-
-
+# Inputs para el gráfico
 var             = "etiquetas"
 tiempo          = "anio"
 label_registros = "defunciones"
@@ -78,43 +73,28 @@ color_destacado = "#fc3f3f"
 titulo1         = "Evolución de las 10 causas de mortalidad más frecuentes"
 titulo2         = "Argentina 2009-2018"
 
-degra <-colorRampPalette(c('lightgreen','lightblue'))(num.vars)
-
 # Grafico de líneas
-ggplot(df, aes(x = get(tiempo), y = n, group = CAUSA)) +
-  geom_line( aes(color = CAUSA), stat = "identity", size = rel(1.4)) + 
+ggplot(df, aes(x = get(tiempo), y = n, group = etiquetas)) +
+  geom_line( aes(color = etiquetas), stat = "identity", size = rel(1.4)) + 
+  #scale_color_brewer(palette = ggthemes::Red, direction = -1) +
+  scale_color_manual(values = c(rev(brewer.pal(9, "Reds")), "#FFF5F0")) + 
   #  scale_fill_manual("") +
-  #scale_x_discrete(name = "Año") + 
+  scale_x_continuous(name = "Año") + 
   scale_y_continuous(name = paste0("Número de ", label_registros)) + 
   theme_bw() +
   geom_hline(yintercept = 0, color = "grey", size = .5) +
   labs(title    = titulo1,
+       subtitle = titulo2,
        caption  = paste0("Fuente: http://www.deis.msal.gov.ar/ \n
                           Realizado por @leokova, 13 May 2020"))  +
-  theme(panel.border     = element_blank(), 
-        panel.grid.minor = element_blank(), 
-        panel.background = element_blank(),
-        plot.title       = element_text(size = rel(1.5)),
-        axis.text.y      = element_text(size = rel(1.3)),
-        axis.text.x      = element_text(size = rel(1.4)),
-        axis.title.x     = element_text(size = rel(1.2)),
-        axis.title.y     = element_text(size = rel(1.2))
+  theme(
+    panel.border     = element_blank(), 
+    panel.grid.minor = element_blank(), 
+    panel.background = element_blank(),
+    plot.title       = element_text(size = rel(1.5)),
+    axis.text.y      = element_text(size = rel(1.3)),
+    axis.text.x      = element_text(size = rel(1.4)),
+    axis.title.x     = element_text(size = rel(1.2)),
+    axis.title.y     = element_text(size = rel(1.2)),
+    legend.title     = element_blank()
   )
-
-
-ggplot(df, aes(x = get(tiempo), y = n, group = CAUSA)) +
-  geom_line( aes(color = CAUSA), stat = "identity", size = rel(1.4)) #+"#f0c836") +
-scale_fill_manual("") +
-  scale_y_continuous(name = "Número de creditos otorgados") + 
-  theme_bw() +   # Quitar el color de fondo
-  geom_hline(yintercept = 0, color = "grey", size = .5) +
-  theme(panel.border = element_blank(),  
-        panel.grid.minor   = element_blank(), 
-        panel.background   = element_blank(),
-        axis.text.y        = element_text(size = rel(1.5)),
-        axis.text.x        = element_text(size = rel(1.5)),
-        axis.title.x       = element_text(size = rel(1.3)),
-        axis.title.y       = element_text(size = rel(1.3))
-  )
-
-
